a50304e030c7da0deb319c1895bf7911f7cf2706
SJ <tkdwox31__@naver.com>
2022-05-30 17:42:01 +0900

    first commit

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PopupPage from "./MainPage/PopupPage";
+import RecommendPage from "./MainPage/RecommendPage";
+import ReviewPage from "./MainPage/ReviewPage";
+import SearchPage from "./MainPage/SearchPage";
+import YoutubePage from "./MainPage/YoutubePage";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <PopupPage />
+      <SearchPage />
+      <RecommendPage />
+      <ReviewPage />
+      <YoutubePage />
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ src/components/NavbarPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavbarPage = () => {
+  return (
+    <div>
+      <h1>nav 바</h1>
+    </div>
+  );
+};
+
+export default NavbarPage;
