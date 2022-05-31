fb989ab5a4c5f55ead590df88f781d998e84bdc4
SJ <tkdwox31__@naver.com>
2022-05-31 13:59:06 +0900

    0531 popup

--- src/components/MainPage/PopupPage.jsx
+++ src/components/MainPage/PopupPage.jsx
@@ -1,15 +1,43 @@
 import React from "react";
 import styled from "styled-components";
-import { Modal, Button } from "antd";
+// import { Modal } from "antd";
+
+const Popup = styled.div`
+  width: 350px;
+  height: 500px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px 5px gray;
+`;
+const PopupImage = styled.img`
+  border-radius: 10px;
+  width: 350px;
+  height: 500px;
+`;
+
+const PopupButton = styled.button`
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  border: none;
+  border-radius: 50%;
+  background: black;
+  opacity: 0.65;
+  color: #fff;
+  font-size: 1.5rem;
+  margin: 15px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
 
 const PopupPage = () => {
   return (
-    <div>
-      <Button type="primary">버튼</Button>
-      <Modal title="제목">
-        <p>내용</p>
-      </Modal>
-    </div>
+    <Popup>
+      <PopupButton>X</PopupButton>
+      <PopupImage src="https://pbs.twimg.com/media/EWwwRz5WsAETaUb.png"></PopupImage>
+    </Popup>
   );
 };
 
