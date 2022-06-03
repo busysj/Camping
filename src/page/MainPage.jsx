ae2b2605530f205dc42d663eef2527c81a803b5d
SJ <tkdwox31__@naver.com>
2022-06-03 13:41:12 +0900

    0603

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import PopupPage from "./MainPage/PopupPage";
-import RecommendPage from "./MainPage/RecommendPage";
-import ReviewPage from "./MainPage/ReviewPage";
-import SearchPage from "./MainPage/SearchPage";
-import YoutubePage from "./MainPage/YoutubePage";
+import PopupPage from "../components/MainPage/PopupPage";
+import RecommendPage from "../components/MainPage/RecommendPage";
+import ReviewPage from "../components/MainPage/ReviewPage";
+import SearchPage from "../components/MainPage/SearchPage";
+import YoutubePage from "../components/MainPage/YoutubePage";
 
 const MainPage = () => {
   const [popup, setPopup] = useState(true);
